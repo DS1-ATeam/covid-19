c14d58626478abd98e689951eb30495b345d8f8f
GoetheDataScientist <35ads3g5q6f4>
2020-06-02 09:52:55 +0200

    add AOK Wido Table C.12

---
+++ code/Datenaufbereitung_wido.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 28 09:45:21 2020
+
+@author: Luis Hohmann
+"""
+
+import pandas as pd
+
+
+df = pd.read_csv(filepath_or_buffer="../data/00_AOK_wido_c_12.csv", sep=';')
